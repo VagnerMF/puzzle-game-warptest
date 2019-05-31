a2798dd8839502671067e08916c72a079bbf504e
Hugh Bagan <hugh.bagan@gmail.com>
2019-05-30 23:59:01 -0600

    Talker text label has responsive margins

--- actors/talker/Talker.gd
+++ actors/talker/Talker.gd
@@ -1,48 +1,66 @@
 class_name Talker extends KinematicBody2D
 
-onready var text := $RichTextLabel
-onready var timer := $TextTimer
 onready var spr_w = $Sprite.get_rect().size.x
 onready var spr_h = $Sprite.get_rect().size.y
 
+onready var label := $RichTextLabel
+onready var label_rect : Rect2 = label.get_rect() # Cache the original pos
+
+onready var timer := $TextTimer
+
 var dialog := [
-	"Hello!",
-	"How are you?",
-	"I'm doing ok.",
-	"Trying to get this damn text to work though"
+	"Trying to get this damn label to work though.",
+	"arblarblarblarblarblarblarblarblarblarblarblarblarblarblarblarblarblarblarbl",
+	"I'm doing ok."
 ]
 var page := 0
-var speak := false
+
 
 func _ready():
-	
 	print(spr_w, spr_h)
-	#text.set_position(Vector2(self.get_position().x, self.get_position().y-(spr_h/2)))
-	#text.set_anchor(Vector2(self.get_position().x, self.get_position().y))
-	#print(text.get_position())
-	
-	text.set_use_bbcode(true)
-	text.set_bbcode(dialog[page])
-	text.set_visible_characters(0)
+	label.set_use_bbcode(true)
+	set_label(dialog[page])
 
-func speak():
+
+func speak() -> void:
 	if timer.is_stopped():
 		timer.start()
 	else:
-		if text.get_visible_characters() < text.get_total_character_count():
-			text.set_visible_characters(text.get_total_character_count())
+		# If the dialog isn't finished typing, just reveal it all
+		if label.get_visible_characters() < label.get_total_character_count():
+			label.set_visible_characters(label.get_total_character_count())
 		else:
-			# Cycle the text
+			# Next page of dialog...
 			if page < dialog.size()-1:
 				page += 1
-				text.set_bbcode(dialog[page])
-				text.set_visible_characters(0)
+				set_label(dialog[page])
 			elif page == dialog.size()-1 :
 				timer.stop()
 				page = 0
-				text.set_bbcode(dialog[page])
-				text.set_visible_characters(0)
+				set_label(dialog[page])
+
+
+func set_label(s:String) -> void:
+	label.set_bbcode("[center]%s[/center]" % s)
+	label.set_visible_characters(0)
+	reposition_label(s)
+
+
+func reposition_label(text:String) -> void:
+	var font:Font = label.get_font("normal_font")
+	var letter_height:float = font.get_height()+1
+	var string_size:= font.get_string_size(text)
+	var lines_needed = ceil(string_size.x / label_rect.size.x)
+	var top_offset = letter_height * (lines_needed+1)
+	label.set_margin(MARGIN_TOP, MARGIN_BOTTOM-top_offset)
+#	print("label_rect.position.y ", label_rect.position.y)
+#	print("letter_height ", letter_height)
+#	print("string_size ", string_size)
+#	print("label_rect ", label_rect.size)
+#	print("lines_needed ", lines_needed)
+#	print("top_offset ", top_offset)
+
 
 func _on_TextTimer_timeout():
-	text.set_visible_characters(text.get_visible_characters()+1)
-	print(text.get_visible_characters())
+	label.set_visible_characters(label.get_visible_characters()+1)
+	#print(label.get_visible_characters())
